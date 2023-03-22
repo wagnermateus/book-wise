b0264a2aa7571e365569700c91cc01be50243ce0
wagnermateus <wagnermateus7@gmail.com>
2023-03-22 12:46:55 +0100

    creation of the prisma adapter and adjustments in authoptions

---
+++ src/lib/auth/prisma-adapter.ts
@@ -0,0 +1,192 @@
+import type { PrismaClient } from "@prisma/client";
+import type { Adapter } from "next-auth/adapters";
+
+export function PrismaAdapter(prisma: PrismaClient): Adapter {
+  return {
+    async createUser(user) {
+      const prismaUser = await prisma.user.create({
+        data: {
+          name: user.name,
+          avatar_url: user.avatar_url,
+          email: user.email,
+        },
+      });
+      return {
+        id: prismaUser.id,
+        name: prismaUser.name,
+        email: prismaUser.email!,
+        emailVerified: null,
+        avatar_url: prismaUser.avatar_url!,
+      };
+    },
+    async getUser(id) {
+      const user = await prisma.user.findUnique({
+        where: {
+          id,
+        },
+      });
+      if (!user) {
+        return null;
+      }
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email!,
+        emailVerified: null,
+        avatar_url: user.avatar_url!,
+      };
+    },
+    async getUserByEmail(email) {
+      const user = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      });
+
+      if (!user) {
+        return null;
+      }
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email!,
+        emailVerified: null,
+        avatar_url: user.avatar_url!,
+      };
+    },
+    async getUserByAccount({ providerAccountId, provider }) {
+      const account = await prisma.account.findUnique({
+        where: {
+          provider_provider_account_id: {
+            provider,
+            provider_account_id: providerAccountId,
+          },
+        },
+        include: {
+          user: true,
+        },
+      });
+      if (!account) {
+        return null;
+      }
+
+      const { user } = account;
+
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email!,
+        emailVerified: null,
+        avatar_url: user.avatar_url!,
+      };
+    },
+
+    async updateUser(user) {
+      const prismaUser = await prisma.user.update({
+        where: {
+          id: user.id!,
+        },
+        data: {
+          name: user.name,
+          email: user.email,
+          avatar_url: user.avatar_url,
+        },
+      });
+      return {
+        id: prismaUser.id,
+        name: prismaUser.name,
+        email: prismaUser.email!,
+        emailVerified: null,
+        avatar_url: prismaUser.avatar_url!,
+      };
+    },
+    async linkAccount(account) {
+      await prisma.account.create({
+        data: {
+          user_id: account.userId,
+          type: account.type,
+          provider: account.provider,
+          provider_account_id: account.providerAccountId,
+          refresh_token: account.refresh_token,
+          access_token: account.access_token,
+          expires_at: account.expires_at,
+          token_type: account.token_type,
+          scope: account.scope,
+          id_token: account.id_token,
+          session_state: account.session_state,
+        },
+      });
+    },
+
+    async createSession({ sessionToken, userId, expires }) {
+      await prisma.session.create({
+        data: {
+          user_id: userId,
+          expires,
+          session_token: sessionToken,
+        },
+      });
+      return {
+        userId,
+        sessionToken,
+        expires,
+      };
+    },
+
+    async getSessionAndUser(sessionToken) {
+      const prismaSession = await prisma.session.findUnique({
+        where: {
+          session_token: sessionToken,
+        },
+        include: {
+          user: true,
+        },
+      });
+
+      if (!prismaSession) {
+        return null;
+      }
+
+      const { user, ...session } = prismaSession;
+
+      return {
+        session: {
+          userId: session.user_id,
+          expires: session.expires,
+          sessionToken: session.session_token,
+        },
+        user: {
+          id: user.id,
+          name: user.name,
+          email: user.email!,
+          emailVerified: null,
+          avatar_url: user.avatar_url!,
+        },
+      };
+    },
+
+    async updateSession({ sessionToken, userId, expires }) {
+      const prismaSession = await prisma.session.update({
+        where: {
+          session_token: sessionToken,
+        },
+        data: {
+          expires,
+          user_id: userId,
+        },
+      });
+      return {
+        sessionToken: prismaSession.session_token,
+        userId: prismaSession.user_id,
+        expires: prismaSession.expires,
+      };
+    },
+    async deleteSession(sessionToken) {
+      await prisma.session.delete({
+        where: {
+          session_token: sessionToken,
+        },
+      });
+    },
+  };
+}
--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -1,8 +1,11 @@
+import { PrismaAdapter } from "@/lib/auth/prisma-adapter";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider, { GoogleProfile } from "next-auth/providers/google";
+import { prisma } from "../../../lib/prisma";
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
@@ -12,9 +15,35 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      authorization: {},
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+      profile(profile: GoogleProfile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          avatar_url: profile.picture,
+        };
+      },
     }),
   ],
+
+  callbacks: {
+    async redirect() {
+      return "/home";
+    },
+    async session({ session, user }) {
+      return {
+        ...session,
+        user,
+      };
+    },
+  },
 };
 
 export default NextAuth(authOptions);
